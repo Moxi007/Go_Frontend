63aab12512ced0012a05542c1d11f2f7bfa7c976
LiFeiyang <lfy16800@gmail.com>
2026-01-02 02:51:13 +0800

    Update cors.go
    仅在 Debug 模式下读取 Body，节省内存。

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,27 +1,21 @@
 package middleware
 
 import (
+	"PiliPili_Frontend/config"
 	"PiliPili_Frontend/logger"
 	"bytes"
 	"github.com/gin-gonic/gin"
 	"io"
 )
 
-// CorsMiddleware handles Cross-Origin Resource Sharing (CORS) headers.
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		logger.Info("Incoming request details:")
-		logger.Info("Request Method: %s", c.Request.Method)
-		logger.Info("Request Path: %s", c.Request.URL.Path)
-		logger.Info("Request Headers: %v", c.Request.Header)
-
-		if c.Request.Method == "POST" || c.Request.Method == "PUT" {
+		// 优化：非 Debug 模式下不要读取 Body
+		if config.GetConfig().LogLevel == "DEBUG" && (c.Request.Method == "POST" || c.Request.Method == "PUT") {
 			body, err := io.ReadAll(c.Request.Body)
-			if err != nil {
-				logger.Error("Error reading request body: %v", err)
-			} else {
+			if err == nil {
 				c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
-				logger.Info("Request Body: %s", body)
+				logger.Debug("Request Body: %s", string(body))
 			}
 		}
 
@@ -29,15 +23,10 @@ func CorsMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		logger.Info("Setting CORS headers for request: %s %s", c.Request.Method, c.Request.URL.Path)
-		logger.Info("Response Headers: %v", c.Writer.Header())
-
 		if c.Request.Method == "OPTIONS" {
-			logger.Error("OPTIONS request received, aborting with status 204")
 			c.AbortWithStatus(204)
 			return
 		}
-
 		c.Next()
 	}
 }
